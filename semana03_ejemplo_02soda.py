2a250fe811e8770511205547472b84d3026b6aec
Dannacmpz <dana.campuzano@ucr.ac.cr>
2026-08-31 02:54:33 +0000

    guardando ejercicios semana3

---
+++ semana03_ejemplo_02soda.py
@@ -0,0 +1,12 @@
+# declarar variables
+monto: float = 0
+impuesto: float = 0
+total: float = 0
+
+monto = float (input("Digite el monto del producto"))
+
+impuesto = monto * 0.13
+total= monto + impuesto
+
+print("Monto: ", monto)
+print("IVA: ", impuesto)
